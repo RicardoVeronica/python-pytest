d3eccff3a4b665a6941bd00e9c5ea51963efee0e
setcain <charlie.bronson00@gmail.com>
2021-06-21 12:01:22 -0500

    init commit

---
+++ add.py
@@ -0,0 +1,18 @@
+"""
+File: app.py
+Author: Ricardo Veronica
+Email: hola@ricardoveronica.com
+Github: https://github.com/ricardoveronica
+Description: Test with pytest
+"""
+
+
+def add(x, y):
+    """
+    Simple function that takes 2 args and adds them.
+    args:
+        x (int/float) first value to add
+        y (int/float) second value to add
+    return x + y
+    """
+    return x + y
---
+++ test_add.py
@@ -0,0 +1,8 @@
+from add import add
+
+
+def test_add():
+    """
+    Test add function
+    """
+    assert add(2, 2) == 4
